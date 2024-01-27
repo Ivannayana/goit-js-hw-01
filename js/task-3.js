78c40d917d513c852a9341ad0262574805dda722
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-01-27 21:34:49 +0200

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
 function getElementWidth(content, padding, border) {
-  content = Number.parseFloat(content);
-  content = Number.parseFloat(padding);
-  content = Number.parseFloat(border);
-  return content + padding * 2 + border * 2;
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+
+  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
